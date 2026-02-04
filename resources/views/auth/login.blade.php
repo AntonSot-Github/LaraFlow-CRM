899163ea7fe012dc4f3f62c4c876678f62fe0a6c
Anton <777elisar777@gmail.com>
2026-02-04 18:09:51 +0100

    Add login page and route

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>LaraFlow — Login</title>
+</head>
+<body>
+
+<h1>Login</h1>
+
+<form>
+    <div>
+        <label>Email</label><br>
+        <input type="email" name="email">
+    </div>
+
+    <div>
+        <label>Password</label><br>
+        <input type="password" name="password">
+    </div>
+
+    <button type="submit">Login</button>
+</form>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', [PageController::class, 'start']);
+Route::get('/login', [AuthController::class, 'loginForm'])->name('auth.login');
